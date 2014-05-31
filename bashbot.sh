858f3d0e1ebdb6cf886b68ea64ab29976e63c087
Donovan Finch <finchd@server.fake>
2014-05-30 19:57:18 -0700

    misc fixes
    fix channelname, fix keepalive, add message parsing.

--- bashbot.sh
+++ bashbot.sh
@@ -8,12 +8,19 @@ tail -f botfile | nc irc.cat.pdx.edu 6667 | while true ; do
     if [ -z $started ] ; then
         echo "USER botbot 0 botbot :I iz a bot" > botfile
         echo "NICK botbot" >> botfile
-        echo "JOIN #notzombies" >> botfile
+        echo "JOIN #notents" >> botfile
         started="yes"
     fi
-    if [ `echo $irc | cut -d ' ' -f 1` = "PING" ] ; then
+    if [ "`echo $irc | cut -d ' ' -f 1`" = "PING" ] ; then
       echo "PONG" >> botfile
     fi
     read irc
-    echo $irc
+    # parse msg fields
+    nick=`echo $irc | cut -d '!' -f 1 | tr -d :`
+    cmd=`echo $irc | cut -d ' ' -f 2`
+    chan=`echo $irc | cut -d ' ' -f 3`
+    message=`echo $irc | cut -d ':' -f 3`
+    if [ "${cmd}" = "PRIVMSG" ] ; then
+      echo "Got message \"${message}\" from ${nick} in ${chan}"
+    fi
 done
