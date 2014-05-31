7277ea48f3a8b21a44271d2afe286f9680d4eb6a
Donovan Finch <finchd@server.fake>
2014-05-30 20:33:49 -0700

    add \!list

--- bashbot.sh
+++ bashbot.sh
@@ -19,7 +19,21 @@ tail -f botfile | nc irc.cat.pdx.edu 6667 | while true ; do
     nick=`echo $irc | cut -d '!' -f 1 | tr -d :`
     cmd=`echo $irc | cut -d ' ' -f 2`
     chan=`echo $irc | cut -d ' ' -f 3`
-    message=`echo $irc | cut -d ':' -f 3`
+    barf=`echo $irc | cut -d ' ' -f 1-3`
+    #message=`echo $irc | cut -d ':' -f 3` #broken by IPv6
+    message=${irc##$barf :}
+    botcommand=`echo $message | cut -d ' ' -f 1`
+    botargs=`$message##$botcommand }
+    # reaction to !cmds
+    if [ "`echo $botcommand | cut -c1`" = '!' ] ; then
+      string="PRIVMSG #notents :Got command \"${botcommand}\" from ${nick} on ${chan} with args \"${botargs}\""
+      case #botcommand in
+        "!add")   list="$botargs $list "
+        "!list")  echo "List is ${list}"
+        "!clear") list=""
+      esac
+    fi
+
     if [ "${cmd}" = "PRIVMSG" ] ; then
       echo "Got message \"${message}\" from ${nick} in ${chan}"
     fi
